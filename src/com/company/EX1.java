252fa0c8e3c1e7e906de1718d12a737ee0c4e9de
tmadurell <madurell95>
2021-02-02 17:20:25 +0100

    EX1

--- src/com/company/EX1.java
+++ src/com/company/EX1.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class EX1 {
     public static void main(String[] args) {
-        System.out.println("Hola soc el arxiu 1");
+        System.out.println("Hola soc el arxiu EX1");
     }
 }
