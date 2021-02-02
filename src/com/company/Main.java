1583260db37b79e11ba8c752988d8b3dd85afbf4
tmadurell <madurell95>
2021-02-02 16:31:33 +0100

    canvi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mon");
-
-        System.out.println("hola mundo");
+        System.out.println("Hola PROFE");
     }
 }
